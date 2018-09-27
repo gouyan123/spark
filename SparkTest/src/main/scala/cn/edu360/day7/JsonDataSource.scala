65255889d88b37c61731da6f29fed2e26988a4d5
gouyan <2637178209@qq.com>
2018-09-27 17:19:35 +0800

    gx

--- SparkTest/src/main/scala/cn/edu360/day7/JsonDataSource.scala
+++ SparkTest/src/main/scala/cn/edu360/day7/JsonDataSource.scala
@@ -2,31 +2,17 @@ package cn.edu360.day7
 
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
-/**
-  * Created by zx on 2017/9/18.
-  */
 object JsonDataSource {
-
   def main(args: Array[String]): Unit = {
-
     val spark = SparkSession.builder().appName("JdbcDataSource")
       .master("local[*]")
       .getOrCreate()
-
     import spark.implicits._
-
-    //指定以后读取json类型的数据(有表头)
-    val jsons: DataFrame = spark.read.json("/Users/zx/Desktop/json")
-
-    val filtered: DataFrame = jsons.where($"age" <=500)
-
-
+    /*指定以后读取json类型的数据(有表头)，如果修改了json文件，要删除该json文件的校验文件；*/
+    val jsons: DataFrame = spark.read.json("D:/json")
+    val filtered: DataFrame = jsons.filter($"age" <=500)
     filtered.printSchema()
-
     filtered.show()
-
     spark.stop()
-
-
   }
 }
