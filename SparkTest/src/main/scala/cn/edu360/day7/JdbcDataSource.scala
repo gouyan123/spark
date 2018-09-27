adb72cc0b89ba5f486262065e4b2fe1afa318fa0
gouyan <2637178209@qq.com>
2018-09-27 16:29:31 +0800

    gx

--- SparkTest/src/main/scala/cn/edu360/day7/JdbcDataSource.scala
+++ SparkTest/src/main/scala/cn/edu360/day7/JdbcDataSource.scala
@@ -32,24 +32,27 @@ object JdbcDataSource {
     val r = logs.filter($"age" <= 13)
     /*如果想使用 sql，注册试图即可*/
     r.show();
-    val reslut: DataFrame = r.select($"sms_id", $"phone", $"age" * 10 as "age")
+    val reslut: DataFrame = r.select($"sms_id",$"content",$"phone", $"age" * 10 as "age")
     reslut.show();
 
-    //val props = new Properties()
-    //props.put("user","root")
-    //props.put("password","123568")
-    //reslut.write.mode("ignore").jdbc("jdbc:mysql://localhost:3306/bigdata", "logs1", props)
-
-    //DataFrame保存成text时出错(只能保存一列)
-    //reslut.write.text("/Users/zx/Desktop/text")
-
-    //reslut.write.json("/Users/zx/Desktop/json")
-
-    //reslut.write.csv("/Users/zx/Desktop/csv")
-
-    //reslut.write.parquet("hdfs://node-4:9000/parquet")
-
-    //reslut.show()
+    /**---*/
+    /**将数据通过jdbc 写入mysql*/
+    val props = new Properties()
+    props.put("user","root")
+    props.put("password","123456")
+    /*ignore表示数据库中已有则忽略，有多种模式*/
+    reslut.write.mode("ignore").jdbc("jdbc:mysql://localhost:3306/test", "sms", props)
+    /**将数据保存到 text文件里*/
+    /*DataFrame保存成文本时，只能保存一列*/
+//    reslut.write.text("D:/text")
+    /**将数据保存到 json里面*/
+    reslut.write.json("D:/json")
+    /**将数据保存到 csv里面，但是没有表头*/
+    reslut.write.csv("D:/csv")
+    /**将数据保存到 parquet里面，parquet是一种特殊的文件格式，不仅用parquet存储，还用snappy压缩了*/
+//    reslut.write.parquet("hdfs://192.168.245.128:9000/parquet")
+    reslut.show()
+    /*关闭连接*/
     spark.close()
   }
 }
